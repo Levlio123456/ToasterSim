148f1f1d6419f6ece1f167a4705057b78d19fcdf
Levlio123456 <113808206+Levlio123456@users.noreply.github.com>
2026-08-03 23:27:57 -0400

    More cleanup
    Currently trying to work out physics, math is hard

--- script.js
+++ script.js
@@ -7,18 +7,22 @@ var keys = {};
 var gamepads = {};
 
 // Vector 2 utilities
+function vector2(x, y) {
+    return {x:x, y:y};
+}
 const vec2 = {
-    add: (v1, v2) => ({x:v1.x+v2.x,y:v1.y+v2.y}),
-    sub: (v1, v2) => ({x:v1.x-v2.x,y:v1.y-v2.y}),
-    mult: (vec, norm) => ({x:vec.x*norm,y:vec.y*norm}),
+    add: (v1, v2) => vector2(v1.x+v2.x,v1.y+v2.y),
+    sub: (v1, v2) => vector2(v1.x-v2.x,v1.y-v2.y),
+    mult: (vec, norm) => vector2(vec.x*norm,vec.y*norm),
     dot: (v1, v2) => v1.x*v2.x  + v1.y*v2.y,
     cross: (v1, v2) => v1.x*v2.y - v1.y*v2.x,
     magSqr: (vec) => Math.pow(vec.x, 2) + Math.pow(vec.y, 2),
     normalize: (vec) => {
         let len = Math.sqrt(Math.pow(vec.x, 2) + Math.pow(vec.y, 2));
-        return len === 0 ? {x:0,y:0} : {x:vec.x/len,y:vec.y/len};
+        return len === 0 ? vector2(0,0) : vector2(vec.x/len,vec.y/len);
     },
-    perpendicular: (vec) => ({x:-vec.y,y:vec.x})
+    perpendicular: (vec) => vector2(-vec.y,vec.x),
+    distance: (v1, v2) => Math.sqrt(Math.pow(v2.x-v1.x, 2) + Math.pow(v2.y-v1.y, 2))
 }
 
 // Input
